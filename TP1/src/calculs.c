3f96a5896b51ca030360411b01ce982ba485b83b
Maxime-esiee-it <maxime.clement@edu.esiee-it.fr>
2026-09-18 09:13:01 +0000

    comit exo 1.8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,62 @@
+#include <stdio.h>
 
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Entrez le premier nombre : ");
+    scanf("%d", &num1);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    if (op != '~') {
+        printf("Entrez le deuxieme nombre : ");
+        scanf("%d", &num2);
+    }
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            else
+                printf("Erreur : division par zero.\n");
+            break;
+
+        case '%':
+            if (num2 != 0)
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            else
+                printf("Erreur : modulo par zero.\n");
+            break;
+
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+
+        default:
+            printf("Operateur non valide.\n");
+    }
+
+    return 0;
+}
